6f46b050238c0d33fc750e9a443a58c36d371b3f
Tainara Novaes <tainarasn.sba@gmail.com>
2024-05-01 12:25:14 -0300

    Updates

--- src/api/index.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-import { url } from "./backend"
-
-export const api = axios.create({
-    // baseURL: "http://localhost:4110/api",
-    baseURL: `http${url}/api`,
-    timeout: 1000 * 10,
-})
--- src/components/TitleUni.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-interface TitleUniProps {
-    title: string
-}
-
-export const TitleUni: React.FC<TitleUniProps> = ({ title }) => {
-    return <h2 style={{}}>{title}</h2>
-}
